49f5bbdd75c11504bf02342440c2c266f02bea38
ycao <162003030379@qq.com>
2015-08-14 18:06:43 +0800

    aa

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by jiayao3 on 2015/8/14.
  */
+$(function(){
+    $(function(){
+        $("#banner").slidesjs({
+            width: 940,
+            height: 528
+        });
+    });
+});
